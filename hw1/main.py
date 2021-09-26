57a3e446683405ec9a044fcac3dad82ca252dfc4
Abdullah Mamun <ab9mamun@gmail.com>
2021-09-26 03:05:20 -0700

    Declaring the interfaces for hw1- ques 5.1 and 5.2

--- hw1/main.py
+++ hw1/main.py
@@ -4,12 +4,49 @@ Created on Mon Sep 20 18:01:54 2021
 
 @author: abdullahal.mamun1
 """
+
+from modelmanager import *
+
 import pandas as pd
 from datamanager import read_data
 
 def main():
+    dryrun = True
     print('hello')
-    traindf, testdf = read_data()
+    #traindf, testdf = read_data()
+    print('Hellos')
+    
+    Xtrain, Ytrain, Xtest, Ytest = None,None,None,None
+
+    
+    #5.1a - standard and binary
+    standard_binary(Xtrain, Ytrain, Xtest, Ytest, 50, "5.1a")
+    pa_binary(Xtrain, Ytrain, Xtest, Ytest, 50, "5.1a")
+    
+    #5.1b,c
+    standard_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1b")
+    pa_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1b")
+    
+    averaged_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1c")
+    
+    #5.1d 
+    general_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1d", dryrun)
+    
+    
+    #5.1a - standard and binary
+    standard_binary(Xtrain, Ytrain, Xtest, Ytest, 50, "5.1a")
+    pa_binary(Xtrain, Ytrain, Xtest, Ytest, 50, "5.1a")
+    
+    #5.1b,c
+    standard_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1b")
+    pa_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1b")
+    
+    averaged_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1c")
+    
+    #5.1d 
+    general_binary(Xtrain, Ytrain, Xtest, Ytest, 20, "5.1d", dryrun)
+    
+    
     
     
     
---
+++ hw1/modelmanager.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Abdullah Mamun
+"""
+
+def standard_binary(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo):
+    print("Running the standard_binary for Q", quesNo)
+
+
+def standard_multiclass(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo):
+    print("Running the standard_multiclass for Q", quesNo)
+
+
+def pa_binary(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo):
+    print("Running the pa_binary for Q", quesNo)
+
+def pa_multiclass(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo):
+    print("Running the pa_multiclass for Q", quesNo)
+
+
+def averaged_binary(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo):
+    print("Running the averaged_binary for Q", quesNo)
+
+def averaged_multiclass(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo):
+    print("Running the averaged_multiclass for Q", quesNo)
+    
+
+def general_binary(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo, dryrun):
+    print("Running the general_binary for Q", quesNo)
+
+
+def general_multiclass(Xtrain, Ytrain, Xtest, Ytest, epochs, quesNo, dryrun):
+    print("Running the general_multiclass for Q", quesNo)
+        
